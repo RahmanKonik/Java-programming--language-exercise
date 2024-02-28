52588c3613e8ea21aaea1809424a9f1dd72dec3d
Konik Rahman <konikrahman06@gmail.com>
2024-02-28 19:14:58 +0200

    java-exercise/helloworld.java: display the hello java world

---
+++ java-exercises/HelloWorld.java
@@ -0,0 +1,17 @@
+
+// Prints the hello world in java program.
+// Name: Konik Rahman.
+// Date: 28-02-2024
+
+/**
+ * Prints the HelloWorld.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String text = "Hello Java World!";
+        System.out.println(text);
+    }
+}
+
+// End of file
