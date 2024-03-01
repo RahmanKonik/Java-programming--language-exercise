53ed16df989ac2ec1b2f58213ad92dc88e414fd1
Konik Rahman <konikrahman06@gmail.com>
2024-03-01 22:40:18 +0200

    java-exercise/ multitable.java: display the 1 to 9 tables in program

---
+++ java-exercises/Multitables.java
@@ -0,0 +1,20 @@
+
+// Write a program that display the 1 to 9 tables .
+// Name: Konik Rahman.
+// Date: 01-03-2024
+
+public class Multitables {
+
+    public static void main(String[] args) {
+
+        System.out.println("Table:- ");
+
+        for ( int i = 1; i < 10; i++){
+            for ( int j = 1; j < 10; j++){
+                System.out.print( i* j +" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
